432782a9202127a3330a307d44dce9f9d2fdebe8
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-08-25 14:28:55 +0300

    create Table.jsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React, {useState} from 'react';
 import './App.css';
 import Modal from './ui/modal/Modal';
 import Search from "./ui/search/Search";
+import Table from "./ui/table/Table";
 
 function App() {
     const [modalActive, setModalActive] = useState(true)
@@ -15,157 +16,158 @@ function App() {
                     <button onClick={() => setModalActive(true)}>Open modal</button>
                 </div>
                 <Search/>
+                <Table/>
 
-                <div className="tab_container">
-                    <table className="table">
-                        <thead>
-                        <tr>
-                            <th>Model</th>
-                            <th>Titile</th>
-                            <th>OEM</th>
-                            <th>Date</th>
-                            <th>Ebay $</th>
-                            <th>Склад $</th>
-                            <th>Link</th>
+                {/*<div className="tab_container">*/}
+                {/*    <table className="table">*/}
+                {/*        <thead>*/}
+                {/*        <tr>*/}
+                {/*            <th>Model</th>*/}
+                {/*            <th>Titile</th>*/}
+                {/*            <th>OEM</th>*/}
+                {/*            <th>Date</th>*/}
+                {/*            <th>Ebay $</th>*/}
+                {/*            <th>Склад $</th>*/}
+                {/*            <th>Link</th>*/}
 
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr>
-                            <td>Audi</td>
-                            <td>Маховик</td>
-                            <td>89815-50020</td>
-                            <td>11.04.2022</td>
-                            <td>200</td>
-                            <td>190</td>
-                            <td>
-                                <a href="https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4">https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4
+                {/*        </tr>*/}
+                {/*        </thead>*/}
+                {/*        <tbody>*/}
+                {/*        <tr>*/}
+                {/*            <td>Audi</td>*/}
+                {/*            <td>Маховик</td>*/}
+                {/*            <td>89815-50020</td>*/}
+                {/*            <td>11.04.2022</td>*/}
+                {/*            <td>200</td>*/}
+                {/*            <td>190</td>*/}
+                {/*            <td>*/}
+                {/*                <a href="https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4">https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4*/}
 
-                                </a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>BMW</td>
-                            <td>Маховик</td>
-                            <td>89815-50020</td>
-                            <td>11.04.2022</td>
-                            <td>200</td>
-                            <td>190</td>
-                            <td>
-                                <a href="https://www.ebay.de/sch/i.html?_from=R40&_nkw=88150-50030&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4">https://www.ebay.de/sch/i.html?_from=R40&_nkw=88150-50030&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4
+                {/*                </a>*/}
+                {/*            </td>*/}
+                {/*        </tr>*/}
+                {/*        <tr>*/}
+                {/*            <td>BMW</td>*/}
+                {/*            <td>Маховик</td>*/}
+                {/*            <td>89815-50020</td>*/}
+                {/*            <td>11.04.2022</td>*/}
+                {/*            <td>200</td>*/}
+                {/*            <td>190</td>*/}
+                {/*            <td>*/}
+                {/*                <a href="https://www.ebay.de/sch/i.html?_from=R40&_nkw=88150-50030&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4">https://www.ebay.de/sch/i.html?_from=R40&_nkw=88150-50030&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4*/}
 
-                                </a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Audi</td>
-                            <td>Маховик</td>
-                            <td>89815-50020</td>
-                            <td>11.04.2022</td>
-                            <td>200</td>
-                            <td>190</td>
-                            <td>
-                                <a href="https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4">https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4
+                {/*                </a>*/}
+                {/*            </td>*/}
+                {/*        </tr>*/}
+                {/*        <tr>*/}
+                {/*            <td>Audi</td>*/}
+                {/*            <td>Маховик</td>*/}
+                {/*            <td>89815-50020</td>*/}
+                {/*            <td>11.04.2022</td>*/}
+                {/*            <td>200</td>*/}
+                {/*            <td>190</td>*/}
+                {/*            <td>*/}
+                {/*                <a href="https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4">https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4*/}
 
-                                </a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Audi</td>
-                            <td>Маховик</td>
-                            <td>89815-50020</td>
-                            <td>11.04.2022</td>
-                            <td>200</td>
-                            <td>190</td>
-                            <td>
-                                <a href="https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4">https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4
+                {/*                </a>*/}
+                {/*            </td>*/}
+                {/*        </tr>*/}
+                {/*        <tr>*/}
+                {/*            <td>Audi</td>*/}
+                {/*            <td>Маховик</td>*/}
+                {/*            <td>89815-50020</td>*/}
+                {/*            <td>11.04.2022</td>*/}
+                {/*            <td>200</td>*/}
+                {/*            <td>190</td>*/}
+                {/*            <td>*/}
+                {/*                <a href="https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4">https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4*/}
 
-                                </a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Audi</td>
-                            <td>Маховик</td>
-                            <td>89815-50020</td>
-                            <td>11.04.2022</td>
-                            <td>200</td>
-                            <td>190</td>
-                            <td>
-                                <a href="https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4">https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4
+                {/*                </a>*/}
+                {/*            </td>*/}
+                {/*        </tr>*/}
+                {/*        <tr>*/}
+                {/*            <td>Audi</td>*/}
+                {/*            <td>Маховик</td>*/}
+                {/*            <td>89815-50020</td>*/}
+                {/*            <td>11.04.2022</td>*/}
+                {/*            <td>200</td>*/}
+                {/*            <td>190</td>*/}
+                {/*            <td>*/}
+                {/*                <a href="https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4">https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4*/}
 
-                                </a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Audi</td>
-                            <td>Маховик</td>
-                            <td>89815-50020</td>
-                            <td>11.04.2022</td>
-                            <td>200</td>
-                            <td>190</td>
-                            <td>
-                                <a href="https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4">https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4
+                {/*                </a>*/}
+                {/*            </td>*/}
+                {/*        </tr>*/}
+                {/*        <tr>*/}
+                {/*            <td>Audi</td>*/}
+                {/*            <td>Маховик</td>*/}
+                {/*            <td>89815-50020</td>*/}
+                {/*            <td>11.04.2022</td>*/}
+                {/*            <td>200</td>*/}
+                {/*            <td>190</td>*/}
+                {/*            <td>*/}
+                {/*                <a href="https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4">https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4*/}
 
-                                </a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Audi</td>
-                            <td>Маховик</td>
-                            <td>89815-50020</td>
-                            <td>11.04.2022</td>
-                            <td>200</td>
-                            <td>190</td>
-                            <td>
-                                <a href="https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4">https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4
+                {/*                </a>*/}
+                {/*            </td>*/}
+                {/*        </tr>*/}
+                {/*        <tr>*/}
+                {/*            <td>Audi</td>*/}
+                {/*            <td>Маховик</td>*/}
+                {/*            <td>89815-50020</td>*/}
+                {/*            <td>11.04.2022</td>*/}
+                {/*            <td>200</td>*/}
+                {/*            <td>190</td>*/}
+                {/*            <td>*/}
+                {/*                <a href="https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4">https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4*/}
 
-                                </a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Audi</td>
-                            <td>Маховик</td>
-                            <td>89815-50020</td>
-                            <td>11.04.2022</td>
-                            <td>200</td>
-                            <td>190</td>
-                            <td>
-                                <a href="https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4">https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4
+                {/*                </a>*/}
+                {/*            </td>*/}
+                {/*        </tr>*/}
+                {/*        <tr>*/}
+                {/*            <td>Audi</td>*/}
+                {/*            <td>Маховик</td>*/}
+                {/*            <td>89815-50020</td>*/}
+                {/*            <td>11.04.2022</td>*/}
+                {/*            <td>200</td>*/}
+                {/*            <td>190</td>*/}
+                {/*            <td>*/}
+                {/*                <a href="https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4">https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4*/}
 
-                                </a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Audi</td>
-                            <td>Маховик</td>
-                            <td>89815-50020</td>
-                            <td>11.04.2022</td>
-                            <td>200</td>
-                            <td>190</td>
-                            <td>
-                                <a href="https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4">https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4
+                {/*                </a>*/}
+                {/*            </td>*/}
+                {/*        </tr>*/}
+                {/*        <tr>*/}
+                {/*            <td>Audi</td>*/}
+                {/*            <td>Маховик</td>*/}
+                {/*            <td>89815-50020</td>*/}
+                {/*            <td>11.04.2022</td>*/}
+                {/*            <td>200</td>*/}
+                {/*            <td>190</td>*/}
+                {/*            <td>*/}
+                {/*                <a href="https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4">https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4*/}
 
-                                </a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Audi</td>
-                            <td>Маховик</td>
-                            <td>89815-50020</td>
-                            <td>11.04.2022</td>
-                            <td>200</td>
-                            <td>190</td>
-                            <td>
-                                <a href="https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4">https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4
+                {/*                </a>*/}
+                {/*            </td>*/}
+                {/*        </tr>*/}
+                {/*        <tr>*/}
+                {/*            <td>Audi</td>*/}
+                {/*            <td>Маховик</td>*/}
+                {/*            <td>89815-50020</td>*/}
+                {/*            <td>11.04.2022</td>*/}
+                {/*            <td>200</td>*/}
+                {/*            <td>190</td>*/}
+                {/*            <td>*/}
+                {/*                <a href="https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4">https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4*/}
 
-                                </a>
-                            </td>
-                        </tr>
-                        </tbody>
-                    </table>
+                {/*                </a>*/}
+                {/*            </td>*/}
+                {/*        </tr>*/}
+                {/*        </tbody>*/}
+                {/*    </table>*/}
 
 
-                </div>
+                {/*</div>*/}
                 {/*<div className="container">*/}
                 {/*  <input type="button" onClick="javascript: FindOnPage('text-to-find'); return false;" value="Find"/>*/}
                 {/*  <input type="text" maxLength="30" placeholder="Search" className="searchbar" id="text-to-find" value="">*/}
---
+++ src/ui/table/Table.jsx
@@ -0,0 +1,161 @@
+import React from 'react';
+import '../../style/Table.module.css'
+const Table = () => {
+    return (
+        <div>
+            <div className="tab_container">
+                <table className="table">
+                    <thead>
+                    <tr>
+                        <th>Model</th>
+                        <th>Titile</th>
+                        <th>OEM</th>
+                        <th>Date</th>
+                        <th>Ebay $</th>
+                        <th>Склад $</th>
+                        <th>Link</th>
+
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>Audi</td>
+                        <td>Маховик</td>
+                        <td>89815-50020</td>
+                        <td>11.04.2022</td>
+                        <td>200</td>
+                        <td>190</td>
+                        <td>
+                            <a href="https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4">https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4
+
+                            </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>BMW</td>
+                        <td>Маховик</td>
+                        <td>89815-50020</td>
+                        <td>11.04.2022</td>
+                        <td>200</td>
+                        <td>190</td>
+                        <td>
+                            <a href="https://www.ebay.de/sch/i.html?_from=R40&_nkw=88150-50030&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4">https://www.ebay.de/sch/i.html?_from=R40&_nkw=88150-50030&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4
+
+                            </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Audi</td>
+                        <td>Маховик</td>
+                        <td>89815-50020</td>
+                        <td>11.04.2022</td>
+                        <td>200</td>
+                        <td>190</td>
+                        <td>
+                            <a href="https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4">https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4
+
+                            </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Audi</td>
+                        <td>Маховик</td>
+                        <td>89815-50020</td>
+                        <td>11.04.2022</td>
+                        <td>200</td>
+                        <td>190</td>
+                        <td>
+                            <a href="https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4">https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4
+
+                            </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Audi</td>
+                        <td>Маховик</td>
+                        <td>89815-50020</td>
+                        <td>11.04.2022</td>
+                        <td>200</td>
+                        <td>190</td>
+                        <td>
+                            <a href="https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4">https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4
+
+                            </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Audi</td>
+                        <td>Маховик</td>
+                        <td>89815-50020</td>
+                        <td>11.04.2022</td>
+                        <td>200</td>
+                        <td>190</td>
+                        <td>
+                            <a href="https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4">https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4
+
+                            </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Audi</td>
+                        <td>Маховик</td>
+                        <td>89815-50020</td>
+                        <td>11.04.2022</td>
+                        <td>200</td>
+                        <td>190</td>
+                        <td>
+                            <a href="https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4">https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4
+
+                            </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Audi</td>
+                        <td>Маховик</td>
+                        <td>89815-50020</td>
+                        <td>11.04.2022</td>
+                        <td>200</td>
+                        <td>190</td>
+                        <td>
+                            <a href="https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4">https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4
+
+                            </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Audi</td>
+                        <td>Маховик</td>
+                        <td>89815-50020</td>
+                        <td>11.04.2022</td>
+                        <td>200</td>
+                        <td>190</td>
+                        <td>
+                            <a href="https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4">https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4
+
+                            </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Audi</td>
+                        <td>Маховик</td>
+                        <td>89815-50020</td>
+                        <td>11.04.2022</td>
+                        <td>200</td>
+                        <td>190</td>
+                        <td>
+                            <a href="https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4">https://www.ebay.de/sch/i.html?_from=R40&_nkw=89815-50020&_sacat=0&_sop=2&rt=nc&LH_ItemCondition=4
+
+                            </a>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+
+
+            </div>
+
+        </div>
+    );
+};
+
+export default Table;
